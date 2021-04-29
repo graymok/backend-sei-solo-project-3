26ee6fa696fbd8fea6db8f6e2797d3ef6e1a743d
Tim Mok <timothyjmok@gmail.com>
2021-04-29 16:26:52 -0400

    backend prep, models, migrations, seeders

---
+++ models/user.js
@@ -0,0 +1,38 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+
+const bcrypt = require('bcrypt')
+
+module.exports = (sequelize, DataTypes) => {
+  class user extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      models.user.belongsTo(models.workstyle)
+      models.user.belongsToMany(models.space, {through: 'reservation'})
+    }
+    verifyPassword(input) {
+      return bcrypt.compareSync(input, this.password)
+    }
+  };
+  user.init({
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    workstyleId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'user',
+  });
+  user.addHook('beforeCreate', (user, options) => {
+    const hashedPassword = bcrypt.hashSync(user.password, 10)
+    user.password = hashedPassword
+  })
+  return user;
+};
